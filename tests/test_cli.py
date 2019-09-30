136f92418bf8281b47c925e8a86eb60fffae66e8
Stefan Pratter <stefan@20c.com>
2019-09-30 17:11:10 +0000

    fix tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,9 +7,9 @@ import ctl
 
 
 
-def test_cli():
+def test_cli(config_dir):
     output = subprocess.check_output(
-        ["ctl ls"], shell=True)
+        ["ctl ls --home {}".format(os.path.join(config_dir, "standard"))], shell=True)
 
     output = u"{}".format(output)
-    assert output.find(u"[usage] ran command: `ls`") > -1
+    assert output.find(u"[usage] ran command: `ls --home") > -1
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,13 +1,13 @@
-
+import os
 import ctl
 
 
-def test_ctr_config_reset():
-    ctlr0 = ctl.Ctl()
+def test_ctr_config_reset(config_dir):
+    ctlr0 = ctl.Ctl(config_dir=os.path.join(config_dir, "standard"))
     git0 = ctlr0.get_plugin("ls")
     git0.config["command"] = "changed"
 
-    ctlr1 = ctl.Ctl()
+    ctlr1 = ctl.Ctl(config_dir=os.path.join(config_dir, "standard"))
     git1 = ctlr1.get_plugin("ls")
     assert git0.config != git1.config
 
