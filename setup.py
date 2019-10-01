df73cf63659ebfdfa129c5cc8ab61032a41f690b
Stefan Pratter <stefan@20c.com>
2019-10-01 07:12:44 +0000

    add python versions to setup.py

--- setup.py
+++ setup.py
@@ -17,6 +17,10 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     packages=find_packages("src"),
     package_dir={"": "src"},
