9b84d1fc1a2b65b23f52423df8ec168e5fd1c783
Matt Griswold <grizz@20c.com>
2021-01-13 13:38:44 -0600

    pyupgrade

--- tests/data/pypi/package/ctl_pypi_test/__init__.py
+++ tests/data/pypi/package/ctl_pypi_test/__init__.py
@@ -4,8 +4,8 @@ import os
 def output():
     path = os.path.dirname(__file__)
     path = os.path.join(path, "data", "ctl_pypi_test.txt")
-    with open(path, "r") as fh:
-        print((fh.read()))
+    with open(path) as fh:
+        print(fh.read())
 
 
 if __name__ == "__main__":
